34b9cae1e32e32b1e08060620694444a78ddc6de
Nikita <main.acr0matic@gmail.com>
2021-05-12 21:05:58 +0300

    feat: доработана шапка

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,6 +2,7 @@
 
 const headerNavigation = document.querySelector('#header .nav--header');
 const headerLinks = headerNavigation.querySelectorAll('.nav__item');
+const headerIcons = document.querySelectorAll('#header .social__icon');
 let sectionSlider = null;
 
 if (window.matchMedia('(min-width: 991px)').matches) {
@@ -17,8 +18,13 @@ if (window.matchMedia('(min-width: 991px)').matches) {
     headerLinks.forEach((item) => item.classList.remove('nav__item--active'));
     if (headerLinks[index] && index !== 0) headerLinks[index].classList.add('nav__item--active');
 
-    if (index > 0) headerNavigation.classList.remove('nav--inverted');
-    else headerNavigation.classList.add('nav--inverted');
+    if (index > 0) {
+      headerNavigation.classList.remove('nav--inverted');
+      headerIcons.forEach((icon) => icon.classList.remove('link__icon--inverted'));
+    } else {
+      headerNavigation.classList.add('nav--inverted');
+      headerIcons.forEach((icon) => icon.classList.add('link__icon--inverted'));
+    }
   });
 }
 
