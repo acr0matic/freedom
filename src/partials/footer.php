bd2d16a3aee653ac0b67c4beb7db33efff3e773e
Nikita <main.acr0matic@gmail.com>
2021-05-18 16:24:27 +0300

    feat: перенос на локальный сервер

---
+++ src/partials/footer.php
@@ -0,0 +1,20 @@
+<!-- Подвал -->
+  <footer>
+
+  </footer>
+
+  <!-- Скрипты -->
+  <!-- build:js -->
+  <script src="scripts/libraries/swiper.js"></script>
+  <script src="scripts/libraries/fslightbox.js"></script>
+  <script src="scripts/libraries/choices.min.js"></script>
+  <script src="scripts/libraries/popper.js"></script>
+  <script src="scripts/libraries/tippy.js"></script>
+  <script src="scripts/libraries/micromodal.js"></script>
+
+  <script src="scripts/script.js"></script>
+  <script src="scripts/slider.js"></script>
+  <!-- endbuild -->
+</body>
+
+</html>
---
+++ src/partials/header.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+  <!-- Базовые мета-теги для поисковиков -->
+  <title>Заголовок</title>
+
+  <!-- Иконки для страницы -->
+  <link rel="shortcut icon" href="img/favicons/favicon.ico" type="image/x-icon">
+  <link rel="icon" sizes="16x16" href="img/favicons/favicon-16x16.png" type="image/png">
+  <link rel="icon" sizes="32x32" href="img/favicons/favicon-32x32.png" type="image/png">
+  <link rel="apple-touch-icon-precomposed" href="img/favicons/apple-touch-icon-precomposed.png">
+  <link rel="apple-touch-icon" href="img/favicons/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="167x167" href="img/favicons/apple-touch-icon-167x167.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon-180x180.png">
+  <link rel="apple-touch-icon" sizes="1024x1024" href="img/favicons/apple-touch-icon-1024x1024.png">
+
+  <!-- Метатеги которые выводят информацию о странице в поисковой запрос -->
+  <meta name="description" content="описание не длиннее 155 символов" />
+  <meta name="keywords" content="мета-теги, шаблон, html, css, acr0matic" />
+
+  <!-- Метатеги для ссылок в социальных сетях -->
+  <meta property="og:locale" content="ru_RU" />
+  <meta property="og:type" content="">
+
+  <meta property="og:title" content="">
+  <meta property="og:description" content="" />
+  <meta property="og:image" content="">
+  <meta property="og:url" content="">
+
+  <!-- Контролирует поведение поисковых систем при индексации страницы -->
+  <meta name="robots" content="index,follow,noodp" />
+
+  <!-- Покраска адресной строки в мобильных Chrome, Firefox OS и Opera -->
+  <meta name="theme-color" content="#4285f4" />
+
+  <!-- Для iOS Safari -->
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="#4285f4">
+
+  <!-- Yandex.Metrika counter -->
+
+  <!-- /Yandex.Metrika counter -->
+
+  <!-- Стили -->
+  <!-- build:css -->
+  <link rel="stylesheet" href="css/style.css" />
+  <!-- endbuild -->
+</head>
+
+<body class="page">
+  <!-- Шапка -->
+  <header id="header">
+    <div class="container">
+      <div class="header">
+        <div class="header__nav">
+          <nav class="nav nav--header nav--inverted">
+            <ul class="nav__list d-flex">
+              <li class="nav__item"><a class="nav__link">Главная</a></li>
+              <li class="nav__item"><a class="nav__link">О проекте</a></li>
+              <li class="nav__item"><a class="nav__link">Новости</a></li>
+              <li class="nav__item"><a class="nav__link">История</a></li>
+              <li class="nav__item"><a class="nav__link">Предложения</a></li>
+              <li class="nav__item"><a class="nav__link">Вопросы</a></li>
+              <li class="nav__item"><a class="nav__link">Контакты</a></li>
+            </ul>
+          </nav>
+        </div>
+        <!-- /.header__nav -->
+
+        <div class="header__social">
+          <div class="social">
+            <a href="https://vk.com/publicspacesvb" target="_blank" class="social__link link me-4">
+              <svg class="social__icon link__icon link__icon--inverted" width="39" height="22" viewBox="0 0 39 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M38.1058 1.48983C38.3783 0.630315 38.1058 0 36.8174 0H32.5492C31.4632 0 30.9669 0.545287 30.6944 1.14418C30.6944 1.14418 28.5244 6.17005 25.4493 9.42883C24.4567 10.3771 24.0032 10.6765 23.4622 10.6765C23.1916 10.6765 22.7985 10.3771 22.7985 9.51756V1.48983C22.7985 0.458411 22.4851 0 21.5801 0H14.8733C14.196 0 13.7873 0.476895 13.7873 0.931608C13.7873 1.90758 15.3248 2.13309 15.4825 4.88355V10.8503C15.4825 12.1571 15.2353 12.3956 14.6904 12.3956C13.2443 12.3956 9.72546 7.3512 7.63712 1.57671C7.23035 0.454714 6.81969 0.00184918 5.72979 0.00184918H1.46359C0.243283 0.00184918 0 0.547136 0 1.14603C0 2.22181 1.44607 7.54529 6.736 14.586C10.2626 19.3937 15.2275 22 19.7506 22C22.4618 22 22.7965 21.4214 22.7965 20.4233V16.7893C22.7965 15.6322 23.0554 15.3993 23.9137 15.3993C24.5443 15.3993 25.6303 15.7024 28.1624 18.0185C31.0545 20.7652 31.5333 21.9982 33.1584 21.9982H37.4246C38.643 21.9982 39.2522 21.4196 38.9018 20.2773C38.5184 19.1405 37.1366 17.4861 35.3032 15.5286C34.3067 14.4122 32.8178 13.2107 32.3643 12.61C31.7318 11.8355 31.9148 11.4935 32.3643 10.8059C32.3663 10.8078 37.5647 3.85397 38.1058 1.48983Z"
+                  fill="white" />
+              </svg>
+            </a>
+
+            <a href="https://instagram.com/publicspace_svb" target="_blank" class="social__link link">
+              <svg class="social__icon link__icon link__icon--inverted" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M16 0C11.6544 0 11.1104 0.0191993 9.40373 0.0959993C7.69707 0.172799 6.53653 0.443734 5.51893 0.840534C4.45641 1.25172 3.49146 1.88024 2.68585 2.68585C1.88024 3.49146 1.25172 4.45641 0.840534 5.51893C0.443734 6.53653 0.174933 7.6992 0.0959994 9.40373C0.017066 11.1083 0 11.6544 0 16C0 20.3456 0.0191993 20.8896 0.0959994 22.5963C0.172799 24.3029 0.443734 25.4635 0.840534 26.4811C1.25172 27.5436 1.88024 28.5085 2.68585 29.3141C3.49146 30.1197 4.45641 30.7483 5.51893 31.1595C6.53653 31.5563 7.6992 31.8251 9.40373 31.904C11.1083 31.9829 11.6544 32 16 32C20.3456 32 20.8896 31.9808 22.5963 31.904C24.3029 31.8272 25.4635 31.5563 26.4811 31.1595C27.5436 30.7483 28.5085 30.1197 29.3141 29.3141C30.1198 28.5085 30.7483 27.5436 31.1595 26.4811C31.5563 25.4635 31.8251 24.3008 31.904 22.5963C31.9829 20.8917 32 20.3456 32 16C32 11.6544 31.9808 11.1104 31.904 9.40373C31.8272 7.69706 31.5563 6.53653 31.1595 5.51893C30.7483 4.45641 30.1198 3.49146 29.3141 2.68585C28.5085 1.88024 27.5436 1.25172 26.4811 0.840534C25.4635 0.443734 24.3008 0.174933 22.5963 0.0959993C20.8917 0.017066 20.3456 0 16 0ZM15.9994 7.78452C14.3746 7.78452 12.7862 8.26635 11.4352 9.16907C10.0842 10.0718 9.03116 11.3549 8.40935 12.8561C7.78754 14.3572 7.62485 16.0091 7.94184 17.6027C8.25884 19.1964 9.04128 20.6602 10.1902 21.8092C11.3392 22.9581 12.803 23.7406 14.3967 24.0576C15.9903 24.3746 17.6422 24.2119 19.1434 23.5901C20.6445 22.9683 21.9276 21.9153 22.8304 20.5642C23.7331 19.2132 24.2149 17.6248 24.2149 16C24.2149 13.8211 23.3493 11.7315 21.8087 10.1908C20.268 8.65007 18.1783 7.78452 15.9994 7.78452ZM15.9994 21.3333C14.9446 21.3333 13.9135 21.0205 13.0364 20.4345C12.1593 19.8484 11.4758 19.0155 11.0721 18.0409C10.6684 17.0664 10.5628 15.9941 10.7686 14.9595C10.9744 13.9249 11.4823 12.9746 12.2282 12.2287C12.9741 11.4829 13.9244 10.9749 14.959 10.7691C15.9935 10.5633 17.0659 10.669 18.0404 11.0726C19.015 11.4763 19.8479 12.1599 20.4339 13.0369C21.02 13.914 21.3328 14.9451 21.3328 16C21.3328 17.4145 20.7709 18.771 19.7707 19.7712C18.7705 20.7714 17.4139 21.3333 15.9994 21.3333ZM26.4601 7.45813C26.4601 8.51852 25.6005 9.37813 24.5401 9.37813C23.4797 9.37813 22.6201 8.51852 22.6201 7.45813C22.6201 6.39775 23.4797 5.53813 24.5401 5.53813C25.6005 5.53813 26.4601 6.39775 26.4601 7.45813Z"
+                  fill="white" />
+              </svg>
+            </a>
+          </div>
+          <!-- /.social -->
+        </div>
+        <!-- /.header__social -->
+
+        <div class="header__mobile">
+          <svg class="header__burger" width="36" height="36" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect y="18" width="36" height="3" rx="1.5" fill="white" />
+            <rect x="16" y="27" width="16" height="3" rx="1.5" fill="white" />
+            <rect y="9" width="16" height="3" rx="1.5" fill="white" />
+            <rect x="25" y="4" width="8" height="3" rx="1.5" transform="rotate(90 25 4)" fill="white" />
+            <circle class="fix" cx="23.5" cy="4.5" r="3" fill="#1E5B60" stroke="white" stroke-width="3" />
+            <circle cx="3" cy="29" r="3" fill="white" />
+            <circle cx="7" cy="28" r="4" fill="white" />
+            <circle cx="10.5" cy="29.5" r="2.5" fill="white" />
+          </svg>
+
+        </div>
+        <!-- /.header__mobile -->
+      </div>
+      <!-- /.header -->
+    </div>
+    <!-- /.container -->
+  </header>
+
+  <div id="mobile-menu">
+    <div class="mobile-menu">
+      <div class="mobile-menu__wrapper">
+        <nav class="nav nav--mobile">
+          <ul class="nav__list">
+            <li class="nav__item"><a href="#hero" class="nav__link">Главная</a></li>
+            <li class="nav__item"><a href="#about" class="nav__link">О проекте</a></li>
+            <li class="nav__item"><a href="#news" class="nav__link">Новости</a></li>
+            <li class="nav__item"><a href="#history" class="nav__link">История</a></li>
+            <li class="nav__item"><a href="#feedback" class="nav__link">Предложения</a></li>
+            <li class="nav__item"><a href="#FAQ" class="nav__link">Вопросы</a></li>
+            <li class="nav__item"><a href="#callback" class="nav__link">Контакты</a></li>
+          </ul>
+        </nav>
+
+        <img src="img/icons/control/close.svg" class="mobile-menu__close" alt="">
+        <img src="img/mobile-image.svg" alt="" class="mobile-menu__image">
+      </div>
+      <!-- /.mobile-menu__wrapper -->
+    </div>
+    <!-- /.mobile-menu -->
+  </div>
+  <!-- #mobile-menu -->
